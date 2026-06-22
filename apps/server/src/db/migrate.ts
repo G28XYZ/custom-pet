6b2b86699f9804061910c49806efe66c4ffd9b87
g28xyz <g28xyz@ya.ru>
2026-06-22 23:47:54 +0700

    очистка перед новой генерацией

--- apps/server/src/db/migrate.ts
+++
@@ -1,5 +0,0 @@
-import { type Db } from './index';
-
-export async function migrate(db: Db) {
-  await db.migrate();
-}
--- apps/server/src/db/schema.ts
+++
@@ -1,9 +0,0 @@
-import { bigint, boolean, pgTable, text } from 'drizzle-orm/pg-core';
-
-export const todos = pgTable('todos', {
-  id: text('id').primaryKey(),
-  text: text('text').notNull(),
-  completed: boolean('completed').notNull().default(false),
-  createdAt: bigint('created_at', { mode: 'number' }).notNull(),
-  updatedAt: bigint('updated_at', { mode: 'number' }).notNull(),
-});
